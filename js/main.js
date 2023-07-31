292f3c6b24b2d4bc83205e412f607dbf27fa98df
RoninsWork <130744166+verygood-agency@users.noreply.github.com>
2023-07-31 16:21:35 +0300

    Добавляю анимации на кнопки и скрипты для прилипающей шапки

--- js/main.js
+++ js/main.js
@@ -33,7 +33,85 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   };
 
-  // КОНЕЦ Аккордеон
+  // прилипающая шапка
+  let lastKnownScrollY = 0;
+  let ticking = false;
+
+  // console.log(lastKnownScrollY);
+  // console.log(ticking);
+
+  function headerChange() {
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (scrollTop > 160) {
+      document.body.classList.add("scroll");
+    } else {
+      document.body.classList.remove("scroll");
+    }
+
+    ticking = false;
+  }
+
+  function onScroll() {
+    lastKnownScrollY = window.scrollY;
+    requestTick();
+  }
+
+  function requestTick() {
+    if (!ticking) {
+      requestAnimationFrame(headerChange);
+    }
+    ticking = true;
+  }
+
+  headerChange();
+  window.addEventListener("scroll", onScroll, { passive: true });
+
+  // Анимация кнопок
+  // Ищем все кнопки с классом .btn-animate
+  var buttons = document.querySelectorAll(".btn-animate");
+
+  buttons.forEach(function (button) {
+    // Добавляем в содержимое кнопок блок .decorate
+    var decoration = document.createElement('span');
+    decoration.classList.add('decorate');
+    button.insertBefore(decoration, button.firstChild);
+
+    button.addEventListener("mouseover", function (e) {
+      var pos = button.getBoundingClientRect();
+      var elem_left = pos.left;
+      var elem_top = pos.top;
+
+      var Xinner = e.clientX - elem_left;
+      var Yinner = e.clientY - elem_top;
+
+      var maxDist = Math.max(
+        Math.sqrt(Math.pow(Xinner, 2) + Math.pow(Yinner, 2)),
+        Math.sqrt(Math.pow(button.clientWidth - Xinner, 2) + Math.pow(Yinner, 2)),
+        Math.sqrt(
+          Math.pow(Xinner, 2) + Math.pow(button.clientHeight - Yinner, 2)
+        ),
+        Math.sqrt(
+          Math.pow(button.clientWidth - Xinner, 2) +
+          Math.pow(button.clientHeight - Yinner, 2)
+        )
+      );
+
+      var decoration = button.querySelector(".decorate");
+      decoration.style.left = Xinner + "px";
+      decoration.style.top = Yinner + "px";
+      decoration.style.width = maxDist * 2 + "px";
+      decoration.style.height = maxDist * 2 + "px";
+    });
+
+    button.addEventListener("mouseout", function (e) {
+      var decoration = button.querySelector(".decorate");
+      decoration.style.width = "0";
+      decoration.style.height = "0";
+    });
+  });
+
+
 });
 
 // техническая часть - УДАЛИТЬ НА ПРОДАКШЕНЕ!
